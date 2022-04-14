31cddf4ae23323d21667b33a8d534d3cb5481260
ShaimaaIbrahim <shaimaa333salama@gmail.com>
2022-04-14 18:01:14 +0200

    init testing

--- lib/screens/my_acount_screen.dart
+++ lib/screens/my_acount_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_testing/screens/home.dart';
 import 'package:flutter_testing/widgets/styled_button.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../widgets/app_bar.dart';
 import '../widgets/dropdown_widget.dart';
@@ -15,6 +18,8 @@ class MyAccountScreen extends StatefulWidget {
 }
 
 class _MyAccountScreenState extends State<MyAccountScreen> {
+  var imageFile = null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,19 +35,24 @@ class _MyAccountScreenState extends State<MyAccountScreen> {
                child: Column(
                  crossAxisAlignment: CrossAxisAlignment.end,
                  children: [
-                   Center(
+                 imageFile==null?   Center(
                      child: CircleAvatar(
                        backgroundImage: NetworkImage('https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?t=st=1649014586~exp=1649015186~hmac=f3a9dce45a5216993cd0c89d474be064de38e915d0b1bf45f115fb15b994d715'),
                        radius: 40,
                      ),
-                   ),
+                   ): Image.file(File(imageFile!.path)),
                    heightSpace(5),
-                   Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: [
-                       Icon(Icons.add_box_rounded, color: Colors.green,),
-                       bold16Text('Edit profile pic')
-                     ],
+                   InkWell(
+                     onTap: (){
+
+                     },
+                     child: Row(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       children: [
+                         Icon(Icons.add_box_rounded, color: Colors.green,),
+                         bold16Text('Edit profile pic')
+                       ],
+                     ),
                    ),
                    heightSpace(20),
                    Column(
@@ -144,5 +154,57 @@ class _MyAccountScreenState extends State<MyAccountScreen> {
        ),
      ),
     );
+  }
+
+  Future<void>_showChoiceDialog(BuildContext context)
+  {
+    return showDialog(context: context,builder: (BuildContext context){
+
+      return AlertDialog(
+        title: Text("Choose option",style: TextStyle(color: Colors.blue),),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: [
+              Divider(height: 1,color: Colors.green,),
+              ListTile(
+                onTap: (){
+                  _openGallery(context);
+                },
+                title: Text("Gallery"),
+                leading: Icon(Icons.account_box,color: Colors.green,),
+              ),
+
+              Divider(height: 1,color: Colors.green,),
+              ListTile(
+                onTap: (){
+                  _openCamera(context);
+                },
+                title: Text("Camera"),
+                leading: Icon(Icons.camera,color: Colors.green,),
+              ),
+            ],
+          ),
+        ),);
+    });
+  }
+  void _openGallery(BuildContext context) async{
+    final pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery ,
+    );
+    setState(() {
+      imageFile = pickedFile!;
+    });
+
+    Navigator.pop(context);
+  }
+
+  void _openCamera(BuildContext context)  async{
+    final pickedFile = await ImagePicker().getImage(
+      source: ImageSource.camera ,
+    );
+    setState(() {
+      imageFile = pickedFile!;
+    });
+    Navigator.pop(context);
   }
 }
